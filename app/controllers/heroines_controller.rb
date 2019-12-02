501040e0537e242461b4e74175c47602841f518a
Joseph Burks <joe_burks_3@yahoo.com>
2019-12-02 10:48:05 -0600

    Before Bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,52 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    Heroine.all.each do |heroine|
+      if params[:heroine][:super_name] == heroine.super_name
+        redirect_to '/heroines/error' and return
+      end
+    end
+
+    Heroine.create({
+      name: params[:heroine][:name],
+      super_name: params[:heroine][:super_name],
+      power_id: params[:heroine][:power_id],
+    })
+    redirect_to '/heroines/'
+  end
+
+  def error
+  end
+
+  def edit
+    @heroine = Heroine.find_by(id: params[:id])
+  end
+
+  def update
+    @heroine = Heroine.find_by(id: params[:id])
+    @heroine.update({
+      name: params[:heroine][:name],
+      super_name: params[:heroine][:super_name],
+      power_id: params[:heroine][:power_id],
+    })
+  end
+
+  def destroy
+    @heroine = Heroine.find_by(id: params[:id])
+    @heroine.destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+
+  get('/heroines/error', { to: 'heroines#error'})
+  resources(:powers)
+  resources(:heroines) 
+  
 end
