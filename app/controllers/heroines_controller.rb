464f19ae01c07538fb78e84979ca40e240f004dd
github username <github email address>
2019-12-02 11:27:03 -0600

    completed

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,21 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new 
+  end
+
+  def create
+    Heroine.create({
+        name: params[:name],
+        super_name: params[:super_name],
+        power_id: params[:power_id]
+    })
+    
+end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,7 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+  def show
+    
+  end
 end
