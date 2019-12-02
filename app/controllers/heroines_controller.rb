651313493628e9fd041b73c1871db6e4739eeaaf
alexvancap <alexvancap@gmail.com>
2019-12-02 10:36:30 -0600

    succeeded untill excersize 7

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = @heroine.power
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    if !Heroine.all.find_by({super_name: params[:heroine][:super_name]})
+      Heroine.create({name: params[:heroine][:name], super_name: params[:heroine][:super_name], power_id: params[:heroine][:power_id]})
+    end
+  end
+
+  #can't get this to work :(
+  def filtered_params
+    params.permit({
+      power: [
+        :name,
+        :super_name,
+        :power_id,
+      ]
+    })
+  end
 end
