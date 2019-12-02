74e435fde9067968030c9bc8f9bbe0121bfe3fc3
kareemiztolive <nelsonsmf@gmail.com>
2019-12-02 11:28:06 -0600

    completed

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,26 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @powers = Power.all
+  end
+
+  def create
+    Heroine.create({
+      name: params[:name],
+      super_name: params[:super_name],
+      power_id: params[:super_power]
+
+    })
+
+    redirect_to '/heroines'
+
+  end
+
+
 end
